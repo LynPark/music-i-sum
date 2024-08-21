8c4e2d715ae4d402eac47b445fd01eadf199fbad
LynPark <hyelyn234@gmail.com>
2024-08-22 00:39:20 +0900

    헤더 수정

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,6 @@ function App() {
         />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
-import GoogleLoginButton from "../Auth/GoogleLoginButton";
 import { useAuthState } from "react-firebase-hooks/auth";
+import GoogleLoginButton from "../Auth/GoogleLoginButton";
 import { auth } from "../../services/Firebase";
-import { signOut } from "firebase/auth";
 import LogoutButton from "../Auth/LogoutButton";
 
-function Header() {
+export default function Header() {
   const [user, error] = useAuthState(auth);
+  
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        {user ? (
-          <>
-            <Link to="/chat">Chat</Link>
-            <Link to="/mypage">My Page</Link>
-          </>
-        ) : null}
-        {user ? <LogoutButton /> : <GoogleLoginButton />}
-      </nav>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h6" component={Link} to="/" sx={{ textDecoration: "none", color: "white" }}>
+            MIS
+          </Typography>
+
+          {user && (
+            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+              <Button
+                component={Link}
+                to="/chat"
+                color="inherit"
+                sx={{ textTransform: "none" }}
+              >
+                Chat
+              </Button>
+              <Button
+                component={Link}
+                to="/mypage"
+                color="inherit"
+                sx={{ textTransform: "none" }}
+              >
+                My Page
+              </Button>
+            </Box>
+          )}
+
+          {user ? <LogoutButton /> : <GoogleLoginButton />}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-export default Header;
