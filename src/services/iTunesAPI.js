5dfca9469a2a7a2609d2292f400a5e41f706e3aa
LynPark <162961673+LynPark@users.noreply.github.com>
2024-08-22 17:06:32 +0900

    iTunesAPI.js 업데이트

--- src/services/iTunesAPI.js
+++ src/services/iTunesAPI.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 export const searchMusic = async (query) => {
+    
+    const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+const URL = `${PROXY}/`;
+    
   try {
     const response = await axios.get(`https://itunes.apple.com/search?term=${query}&media=music`);
     return response.data.results;
